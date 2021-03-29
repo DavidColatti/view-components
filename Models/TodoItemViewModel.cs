6bad0af44b170fb261bade5c240da6944e5601a3
David Colatti <davidcolatti@gmail.com>
2021-03-29 12:45:36 -0400

    feat: playing around with view components

---
+++ Models/TodoItemViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace View.Components.Models
+{
+    public class TodoItemViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ ViewComponents/PriorityListViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using View.Components.Models;
+
+namespace View.Components.ViewComponents
+{
+    public class PriorityListViewComponent : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync(string param)
+        {
+            var items = GetItems(param);
+            return View(items);
+        }
+
+        private List<TodoItemViewModel> GetItems(string param)
+        {
+            var items = new List<TodoItemViewModel>()
+            {
+                new TodoItemViewModel {Id = 1, Title = $"Todo: {param}"},
+            };
+
+            return items;
+        }
+    }
+}
